1271dc2e06fa96e6be4f2b4c773fbdb5f9519b4c
Michael Fuery <michaelf@zoosk.com>
2013-11-10 13:38:16 -0800

    visitor metrics saved to db

--- client/page.js
+++ client/page.js
@@ -6,9 +6,11 @@ Template.visitorPage.rendered = function() {
     var visitor_id = Cookie.get(AppCookie.visitor_id);
     var page_id = Session.get('page_id');
     Meteor.call('visitor.update', visitor_id, page_id, function(err, visitor_id) {
-        Cookie.set(AppCookie.visitor_id, visitor_id);
-        console.log('yes')
-        console.log(Visitors.find().fetch());
-        console.log(visitor_id);
+        if (Cookie.get(AppCookie.visitor_id)) {
+            console.log('welcome back, visitor ' + visitor_id);
+        } else {
+            Cookie.set(AppCookie.visitor_id, visitor_id);
+            console.log('hello, new visitor ' + visitor_id);
+        }
     });
 }
